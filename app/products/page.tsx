272b23306ff3de9b9779b1752554d9688c99323b
Igor Henrique <igorhenrique102@gmail.com>
2026-02-05 22:09:10 -0300

    feat: adiciona a página principal

---
+++ app/products/page.tsx
@@ -0,0 +1,9 @@
+export default function Products() {
+    return (
+        <>
+            <button className="bg-green-500 text-white px-4 py-2 rounded cursor-pointer hover:bg-green-500/80 transition-colors">
+                Enviar
+            </button>
+        </>
+    )
+}
